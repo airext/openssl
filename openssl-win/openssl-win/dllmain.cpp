723066a7e76a37eac8f2b08499a2b7e3d17fcc34
Max Rozdobudko <max.rozdobudko@gmail.com>
2020-02-19 16:42:24 +0200

    Fixes string functions for Windows

--- openssl-win/openssl-win/dllmain.cpp
+++ openssl-win/openssl-win/dllmain.cpp
@@ -210,7 +210,7 @@ extern "C" {
     }
 
     FREObject ANXOpenBuildVersion(FREContext context, void* functionData, uint32_t argc, FREObject argv[]) {
-        return ANXOpenSSLConversionRoutines::convertCharArrayToFREObject("2");
+        return ANXOpenSSLConversionRoutines::convertCharArrayToFREObject("3");
     }
 
 #pragma endregion
