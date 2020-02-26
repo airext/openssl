a44035c2524bfce08cd222ca17ddf261ada6fbde
Max Rozdobudko <max.rozdobudko@gmail.com>
2020-02-26 17:30:54 +0200

    Should fix #3 Corrects disposing EVP_MD_CTX object, adds freeing buffer for computed SHA digest. Also adds memory test for computeSHA256 function in debug app.

--- openssl-xcode/OpenSSL/ANXOpenSSLTest/TestSHA.m
+++ openssl-xcode/OpenSSL/ANXOpenSSLTest/TestSHA.m
@@ -9,17 +9,26 @@
 #import "ANXOpenSSL.h"
 #import "data.bin.h"
 
+static NSTimer* _shaTimer;
+
 @implementation TestSHA
 
 + (void)test_ane {
-    unsigned char *plaintext = (unsigned char *)"abc123abc123abc123abc123abc123abc123abc123abc123";
-
-//    unsigned char *digest = [ANXOpenSSL.sharedInstance sha256FromString:plaintext];
-    uint32_t length;
-    unsigned char *digest = [ANXOpenSSL.sharedInstance sha256FromBytes:data_bin inputLength:data_bin_len outputLength:&length];
-
-    NSLog(@"digest is %s", digest);
-    NSLog(@"digest length is %lu", length);
+    if (_shaTimer) {
+        [_shaTimer invalidate];
+        _shaTimer = nil;
+    } else {
+        _shaTimer = [NSTimer scheduledTimerWithTimeInterval:0.001 repeats:YES block:^(NSTimer * _Nonnull timer) {
+            for (NSInteger i = 0; i < 200; i++) {
+                uint32_t length;
+                unsigned char *digest = [ANXOpenSSL.sharedInstance sha256FromBytes:data_bin inputLength:data_bin_len outputLength:&length];
+                if (digest) {
+                    free(digest);
+                }
+            }
+            NSLog(@"tick");
+        }];
+    }
 }
 
 @end
